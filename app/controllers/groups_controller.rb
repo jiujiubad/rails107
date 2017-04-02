24911174f226c8fe53a783bccd19da3deaea13ed
谢育初 <524945638@qq.com>
2017-04-02 18:49:46 +0800

    add validation to title

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -17,14 +17,21 @@ class GroupsController < ApplicationController
 
   def create
     @group = Group.new(group_params)
-    @group.save
-    redirect_to groups_path
+
+    if @group.save
+      redirect_to groups_path
+    else
+      render :new
+    end
   end
 
   def update
     @group = Group.find(params[:id])
-    @group.update(group_params)
-    redirect_to groups_path, notice: "Update Successs"
+    if @group.update(group_params)
+      redirect_to groups_path, notice: "Update Successs"
+    else
+      rendeer :edit
+    end
   end
 
   def destroy
