a6a440cb2ea5487aadfaa482965ac425e70109f4
谢育初 <524945638@qq.com>
2017-04-02 18:54:05 +0800

    add validation to title apply to tdit/update

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -27,10 +27,11 @@ class GroupsController < ApplicationController
 
   def update
     @group = Group.find(params[:id])
+
     if @group.update(group_params)
       redirect_to groups_path, notice: "Update Successs"
     else
-      rendeer :edit
+      render :edit
     end
   end
 
